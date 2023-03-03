06a9628afa920d584b298c359507bfa7218c674a
Jan-Klaas Kesteloot <janklaas.kesteloot@skyline.be>
2023-03-03 17:29:09 +0100

    initial version

---
+++ MyDataMinerAutomationScript_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyDataMinerAutomationScript_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("MyDataMinerAutomationScript_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("947B0DF2-C6E9-4276-80B8-90AF736D2D2C")]
+[assembly: AssemblyVersion("1.0.0.0")]
